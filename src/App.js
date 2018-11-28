66902f997dc82bb5bc6655aa61cd21b85aef8bc5
YaroslavW <doctor355@yahoo.com>
2018-11-28 13:39:23 +0300

    App and Component setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import BarChart from './Components/BarChart';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      data: [100, 400, 232, 280, 630, 120, 78, 262, 380, 30]
+    }
+  }
   render() {
     return (
       <div className="App">
-        My Chart
+        <BarChart data={this.state.date} />
       </div>
     );
   }
